c4f450c05aadd9d336aff7ac5e1c3256f8129106
Arailym <arailym.bakenovaa@gmail.com>
2022-11-13 11:40:54 +0600

    minor changes

--- Qorgan/TabBar/ContactsVC/ContactsViewController.swift
+++ Qorgan/TabBar/ContactsVC/ContactsViewController.swift
@@ -20,8 +20,6 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.delegate = self
         tableView.dataSource = self
         
-        
-        
         fetchAllContacts()
     }
     
@@ -55,20 +53,22 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
     
     //  MARK: - TableView functions
         
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return contacts.count
-        }
-        
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! ContactTableViewCell
-//            cell.imgQ.image = UIImage(named: "ic")
-            cell.name.text = contacts[indexPath.row].givenName
-            cell.num.text = contacts[indexPath.row].number
-            return cell
-        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! ContactTableViewCell
+        cell.name.text = contacts[indexPath.row].givenName
+        cell.num.text = contacts[indexPath.row].number
+        return cell
+    }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
